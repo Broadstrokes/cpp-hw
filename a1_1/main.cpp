45089d678c8f095bc457fc39972b9d99b5bac200
Dan Sajjad <dan.sajjad@gmail.com>
2018-06-13 21:52:13 -0700

    Initial Commit

---
+++ a1_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  a1_1
+//
+//  Created by Dan Sajjad on 6/13/18.
+//  Copyright © 2018 Dan Sajjad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
